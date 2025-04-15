9921c0a7965ce8fbe4af2cf1a2ee9321c2ab3a2b
gdarquie <gaetan.darquie@gmail.com>
2025-04-15 07:41:47 +0200

    ✨ Ready to extract some content from not

--- src/main.rs
+++ src/main.rs
@@ -2,7 +2,9 @@ use regex::Regex;
 use std::path::PathBuf;
 use std::{fs, io};
 
-const FOLDER_LIMIT: usize = 1000;
+const FOLDERS_LIMIT: usize = 1000;
+const FILES_LIMIT: usize = 100000;
+const NOT_PATH: &str = "/home/gaetan/not";
 
 fn get_parent_folders_pathes(parent_folders: Vec<PathBuf>) -> Vec<PathBuf> {
     let re = Regex::new(r"^\d+$").unwrap(); // Matches strings that are only digits
@@ -25,8 +27,8 @@ fn get_folders_pathes(folder_path: PathBuf) -> Result<Vec<PathBuf>, String> {
     let mut stack = vec![folder_path];
 
     while let Some(current) = stack.pop() {
-        if folders.len() >= FOLDER_LIMIT {
-            return Err(format!("Folder limit exceeded ({})", FOLDER_LIMIT));
+        if folders.len() >= FOLDERS_LIMIT {
+            return Err(format!("Folder limit exceeded ({})", FOLDERS_LIMIT));
         }
 
         if current.is_dir() {
@@ -49,9 +51,32 @@ fn get_folders_pathes(folder_path: PathBuf) -> Result<Vec<PathBuf>, String> {
     Ok(folders)
 }
 
-fn main() -> io::Result<()> {
-    const NOT_PATH: &str = "/home/gaetan/not";
+fn get_files_from_path(path: PathBuf) -> Result<Vec<PathBuf>, String> {
+    let mut files = Vec::new();
+    let mut stack = vec![path];
 
+    while let Some(current) = stack.pop() {
+        if current.is_dir() {
+            match fs::read_dir(&current) {
+                Ok(entries) => {
+                    for entry in entries.flatten() {
+                        let path = entry.path();
+                        if path.is_dir() {
+                            stack.push(path);
+                        } else {
+                            files.push(path);
+                        }
+                    }
+                }
+                Err(err) => eprintln!("Failed to read {}: {}", current.display(), err),
+            }
+        }
+    }
+
+    Ok(files)
+}
+
+fn main() -> io::Result<()> {
     let mut parent_folders = fs::read_dir(NOT_PATH)?
         .map(|parent_folder| parent_folder.map(|e| e.path()))
         .collect::<Result<Vec<_>, io::Error>>()?;
@@ -65,12 +90,28 @@ fn main() -> io::Result<()> {
         .flat_map(|folder| get_folders_pathes(folder.clone()).unwrap_or_default())
         .collect();
 
-    // create a list of all the files in this folders
-
-    // parse all this files
-
-    all_folders
+    // put all files in a vector
+    let all_files: Vec<PathBuf> = all_folders
+        // todo: limit the number of files to 100000
         .iter()
-        .for_each(|entry| println!("An entry {}", entry.display()));
+        .flat_map(|folder| get_files_from_path(folder.clone()).unwrap_or_default())
+        .take(FILES_LIMIT)
+        .collect();
+
+    // print the number of files
+    println!("Number of files: {}", all_files.len());
+
+    // print the number of lines and words by file
+    all_files.iter().for_each(|file| {
+        let lines = fs::read_to_string(file).unwrap_or_default();
+        let line_count = lines.lines().count();
+        let word_count = lines.split_whitespace().count();
+
+        println!("File: {} - Number of lines: {}", file.display(), line_count);
+        println!("File: {} - Number of words: {}", file.display(), word_count);
+    });
+
+    // todo: parse every files and extract some content
+
     Ok(())
 }
