e1eb78840b6b50a38fb17769bd727922c648ebd4
gdarquie <gaetan.darquie@gmail.com>
2025-05-15 09:40:03 +0200

    🚧 Refactoring how to get files with one unique function get_files_pathes

---
+++ src/libs/commands.rs
@@ -0,0 +1,79 @@
+use crate::files::get_not_files_pathes;
+use std::{fs, io};
+
+pub fn print_commands() {
+    eprintln!("Available commands:");
+    eprintln!("  stats                Get stats from not files");
+    eprintln!("  extract <keyword>    Extract content with a specific keyword");
+    eprintln!("  append               Append content to a file");
+}
+
+pub fn compute_stats(not_path: &str, files_limit: &usize) -> io::Result<()> {
+    let all_files = get_not_files_pathes(&not_path, files_limit)?;
+
+    println!("Number of files: {}", all_files.len());
+
+    all_files.iter().for_each(|file| {
+        let lines = fs::read_to_string(file).unwrap_or_default();
+        let line_count = lines.lines().count();
+        let word_count = lines.split_whitespace().count();
+
+        println!("File: {} - Number of lines: {}", file.display(), line_count);
+        println!("File: {} - Number of words: {}", file.display(), word_count);
+    });
+
+    Ok(())
+}
+
+pub fn extract(keyword: &str) -> io::Result<()> {
+    println!("Extracting content with keyword: {}", keyword);
+    // TODO: Implement extraction logic
+
+    // use lib and get all folders then files
+    // for each file, check if the keyword is in the file
+    // if it is, get the last extract and append it
+    // to the last not file
+    Ok(())
+}
+
+pub fn append(not_path: &str, files_limit: &usize) -> io::Result<()> {
+    println!("Appending content to the last `.md` file in the not folder...");
+
+    // Find all `.md` files in the NOT_PATH directory
+    let mut md_files = get_not_files_pathes(not_path, files_limit)?;
+
+    // Sort the files based on the numeric representation of their paths
+    md_files.sort_by_key(|path| {
+        path.to_string_lossy()
+            .replace("/", "") // Remove all slashes
+            .replace(".md", "") // Remove the `.md` extension
+            .parse::<u64>() // Parse the resulting string as a number
+            .unwrap_or(0) // Use 0 if parsing fails
+    });
+
+    // Get the last `.md` file
+    if let Some(last_md_file) = md_files.last() {
+        println!("Found last `.md` file: {}", last_md_file.display());
+
+        // Append content to the file
+        use std::fs::OpenOptions;
+        use std::io::Write;
+
+        let mut file = OpenOptions::new()
+            .write(true)
+            .append(true)
+            .open(last_md_file)?;
+
+        writeln!(file, "[//]: # \"extract-start-test\"")?;
+        writeln!(file, "\n")?;
+        writeln!(file, "[//]: # \"extract-end-test\"")?;
+        println!(
+            "Content appended successfully to {}",
+            last_md_file.display()
+        );
+    } else {
+        println!("No `.md` files found in the not folder.");
+    }
+
+    Ok(())
+}
---
+++ tests/dispatcher_test.rs
@@ -0,0 +1,63 @@
+use nost::dispatcher::{dispatch, print_commands};
+use std::fs::File;
+use std::io::Write;
+use tempfile::tempdir;
+
+#[test]
+fn test_print_commands() {
+    let output = std::panic::catch_unwind(|| {
+        print_commands();
+    });
+
+    assert!(output.is_ok());
+}
+
+#[test]
+fn test_dispatch_stats() {
+    let temp_dir = tempdir().unwrap();
+    let file_path = temp_dir.path().join("file.txt");
+    let mut file = File::create(&file_path).unwrap();
+    writeln!(file, "Hello, world!").unwrap();
+
+    let args = vec!["program".to_string(), "stats".to_string()];
+    let not_path = temp_dir.path().to_str().unwrap();
+
+    let result = dispatch(&args, not_path);
+    assert!(result.is_ok());
+}
+
+// todo: check that when arg is "extract", it cal the extract function
+#[test]
+fn test_dispatch_extract() {
+    let temp_dir = tempdir().unwrap();
+    let args = vec![
+        "program".to_string(),
+        "extract".to_string(),
+        "keyword".to_string(),
+    ];
+    let not_path = temp_dir.path().to_str().unwrap();
+
+    let result = dispatch(&args, not_path);
+    assert!(result.is_ok());
+}
+
+// todo: check that when arg is "append", it cal the append function
+#[test]
+fn test_dispatch_append() {
+    let temp_dir = tempdir().unwrap();
+    let args = vec!["program".to_string(), "append".to_string()];
+    let not_path = temp_dir.path().to_str().unwrap();
+
+    let result = dispatch(&args, not_path);
+    assert!(result.is_ok());
+}
+
+#[test]
+fn test_dispatch_unknown() {
+    let temp_dir = tempdir().unwrap();
+    let args = vec!["program".to_string(), "unknown".to_string()];
+    let not_path = temp_dir.path().to_str().unwrap();
+
+    let result = dispatch(&args, not_path);
+    assert!(result.is_ok());
+}
